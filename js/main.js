5b8f5c2bcdf81e0ecea07e229e8524dd16919a9a
Tatiana Cohen <tatiana.cohen.arias@gmail.com>
2015-02-23 17:50:43 -0500

    Updated JS

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
-/*function showClicks (){
+var heading = document.getElementById("click");
+var count = 0;
+
+heading.addEventListener ("click" , showClicks, false);
+
+function showClicks(){
+	count++;
 	var newPara = document.createElement("p");
-	newPara.innerHTML = "New Paragraph";
-
-	document.body.appendChild(newPara);
-}
-
-var theHeading = document.getElementsByTagName("h1");
-theHeading.addEventListener("click" , showClicks, true);*/
-
-/*var heading = document.getElementsByTagName("h1");
-
-heading.addEventListener ("click" , function showClicks(){
-	var newPara = document.createElement("p");
-	newPara.innerHTML = "New Paragraph";
-	document.body.appendChild(newPara);
-});*/
-
-/*var heading = document.getElementsByTagName("h1");
-
-heading.addEventListener("click" , function(){
-	var newPara = document.createElement("p");
-	newPara.innerHTML = "New Paragraph";
-	document.body.appendChild(newPara);
-});*/
-
-
-var headings = document.getElementsByTagName("h1");
-
-headings.onclick = function () {
-	console.log("you clicked me!");
-}
+		newPara.innerHTML = "This is click number " + count;
+		document.body.appendChild(newPara);
+};
